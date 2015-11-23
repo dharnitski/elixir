418c452520bc451dab3358000bc5070fb7664c14
Dmitry Harnitski <dmitry@brainjocks.com>
2015-11-23 00:40:42 -0500

    Persistent Storage

---
+++ first/test/kv_test.exs
@@ -0,0 +1,62 @@
+# from http://elixir-lang.org/getting-started/processes.html
+defmodule TestKV do
+  use ExUnit.Case
+  doctest KV
+
+  test "receive" do
+    send self(), {:hello, "world"}
+    #{:hello, "world"}
+    receive do
+      {:hello, msg} -> msg
+    after
+      0_000 -> assert false, ":hello should be received"
+    end
+  end
+
+  test "receive atom" do
+    send self(), {:hello}
+    #{:hello, "world"}
+    receive do
+      {:hello} -> "success"
+    after
+      0_000 -> assert false, ":hello should be received"
+    end
+  end
+
+  test "stores key-values" do
+    parent = self()
+    {:ok, pid} = KV.start_link
+    send pid, {:put, :hello, :world}
+    #PID<0.62.0>
+    send pid, {:get, :hello, parent}
+    #{:get, :hello, #PID<0.41.0>}
+
+    assert Map.get(Map.put(%{}, :hello, :world), :hello) == :world
+
+    #works
+    #Task.start_link(fn -> send parent, :world end)
+
+    #works
+    #send self(), :world
+
+    #works
+    #spawn fn -> send(parent, :world) end
+
+    receive do
+      :world -> "Got data from persistent storage"
+    after
+      0_100 -> assert false, ":world should be in main process mainbox"
+    end
+  end
+
+  test "store key-values using Agent" do
+    {:ok, pid} = Agent.start_link(fn -> %{} end)
+    #{:ok, #PID<0.72.0>}
+    Agent.update(pid, fn map -> Map.put(map, :hello, :world) end)
+    #:ok
+    actual = Agent.get(pid, fn map -> Map.get(map, :hello) end)
+
+    assert actual == :world
+  end
+
+end
---
+++ first/test/task_test.exs
@@ -0,0 +1,5 @@
+# from http://elixir-lang.org/getting-started/modules.html
+defmodule TestTask do
+  use ExUnit.Case
+
+end
